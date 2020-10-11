32bcd9582b6674c77b83c0ff1b464784fa6c07eb
DavidTran <62191037+TranDavid1@users.noreply.github.com>
2020-10-11 16:14:01 -0700

    BruteForce.java
    Minor changes in readability of code

--- BruteForce.java
+++ BruteForce.java
@@ -27,7 +27,7 @@ public class BruteForce
         int n = arr.length;
         for (int i = 0; i < n; i++)
             arr[i] = rand.nextInt(200) - 100 ;    // Randomly generate int value between -100 and 100.
-        Arrays.sort(arr);
+        //Arrays.sort(arr);
     }   // end fillArray
 
     public static void main(String[] args)
@@ -54,37 +54,41 @@ public class BruteForce
 
         System.out.println("The random sum between -200 and 200 is: " + randomSum + ".\n");
 
-        int[] values1 = new int[2];                     // Create a new array to store the possible elements
+        /* Create a new array to store the possible elements from each array, take elapsed time in nanoseconds
+           by keeping track of start and end time and subtracting them both, find the two values with bruteForce()
+           method.
+         */
+        int[] values1 = new int[2];
         long nano_array1_startTime = System.nanoTime();
-        values1 = ob.bruteForce(array1, randomSum);     // Set the new array's elements to the elements found in bruteForce
+        values1 = ob.bruteForce(array1, randomSum);
         long nano_array1_endTime = System.nanoTime();
         System.out.println("Numbers found in array of 100 elements: " + values1[0] + " and " + values1[1] + ".");
         System.out.println("Elapsed time (ns) for 100 elements: " + (nano_array1_endTime-nano_array1_startTime) + "\n");
 
-        int[] values2 = new int[2];                     // Create a new array to store the possible elements
+        int[] values2 = new int[2];
         long nano_array2_startTime = System.nanoTime();
-        values2 = ob.bruteForce(array2, randomSum);     // Set the new array's elements to the elements found in bruteForce
+        values2 = ob.bruteForce(array2, randomSum);
         long nano_array2_endTime = System.nanoTime();
         System.out.println("Numbers found in array of 1,000 elements: " + values2[0] + " and " + values2[1] + ".");
         System.out.println("Elapsed time (ns) for 1,000 elements: " + (nano_array2_endTime-nano_array2_startTime) + "\n");
 
-        int[] values3 = new int[2];                     // Create a new array to store the possible elements
+        int[] values3 = new int[2];
         long nano_array3_startTime = System.nanoTime();
-        values3 = ob.bruteForce(array3, randomSum);     // Set the new array's elements to the elements found in bruteForce
+        values3 = ob.bruteForce(array3, randomSum);
         long nano_array3_endTime = System.nanoTime();
         System.out.println("Numbers found in array of 10,000 elements: " + values3[0] + " and " + values3[1] + ".");
         System.out.println("Elapsed time (ns) for 10,000 elements: " + (nano_array3_endTime-nano_array3_startTime) + "\n");
 
-        int[] values4 = new int[2];                     // Create a new array to store the possible elements
+        int[] values4 = new int[2];
         long nano_array4_startTime = System.nanoTime();
-        values4 = ob.bruteForce(array4, randomSum);     // Set the new array's elements to the elements found in bruteForce
+        values4 = ob.bruteForce(array4, randomSum);
         long nano_array4_endTime = System.nanoTime();
         System.out.println("Numbers found in array of 100,000 elements: " + values4[0] + " and " + values4[1] + ".");
         System.out.println("Elapsed time (ns) for 100,000 elements: " + (nano_array4_endTime-nano_array4_startTime) + "\n");
 
-        int[] values5 = new int[2];                     // Create a new array to store the possible elements
+        int[] values5 = new int[2];
         long nano_array5_startTime = System.nanoTime();
-        values5 = ob.bruteForce(array5, randomSum);     // Set the new array's elements to the elements found in bruteForce
+        values5 = ob.bruteForce(array5, randomSum);
         long nano_array5_endTime = System.nanoTime();
         System.out.println("Numbers found in array of 1,000,000 elements: " + values5[0] + " and " + values5[1] + ".");
         System.out.println("Elapsed time (ns) for 1,000,000 elements: " + (nano_array5_endTime-nano_array5_startTime));
