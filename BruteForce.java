c9fea482e31527f17015338ed7ea4fde37989e04
DavidTran <62191037+TranDavid1@users.noreply.github.com>
2020-10-11 17:54:30 -0700

    Changes to BruteForce.java
    Changes to source code

--- BruteForce.java
+++ BruteForce.java
@@ -16,7 +16,7 @@ public class BruteForce
                 }
             }
         }
-        throw new IllegalArgumentException("No two sum solution found.");
+        throw new IllegalArgumentException("No pair found for the targeted sum in the array.");
     } // end bruteForce
 
     // Function to fill out an array with random values, sort the array afterwards
@@ -27,7 +27,7 @@ public class BruteForce
         int n = arr.length;
         for (int i = 0; i < n; i++)
             arr[i] = rand.nextInt(200) - 100 ;    // Randomly generate int value between -100 and 100.
-        //Arrays.sort(arr);
+        Arrays.sort(arr);
     }   // end fillArray
 
     public static void main(String[] args)
@@ -40,10 +40,10 @@ public class BruteForce
 
         //Allocate memory for arrays
         int[] array1 = new int[100];
-        int[] array2 = new int[1000];
-        int[] array3 = new int[10000];
-        int[] array4 = new int[100000];
-        int[] array5 = new int[1000000];
+        int[] array2 = new int[100];
+        int[] array3 = new int[100];
+        int[] array4 = new int[100];
+        int[] array5 = new int[100];
 
         //Fill arrays with random values
         fillArray(array1);
@@ -54,9 +54,10 @@ public class BruteForce
 
         System.out.println("The random sum between -200 and 200 is: " + randomSum + ".\n");
 
-        /* Create a new array to store the possible elements from each array, take elapsed time in nanoseconds
-           by keeping track of start and end time and subtracting them both, find the two values with bruteForce()
-           method.
+        /*
+        Create a new array to store the possible elements from each array, take elapsed time in nanoseconds
+        by keeping track of start and end time and subtracting them both, find the two values with bruteForce()
+        method.
          */
         int[] values1 = new int[2];
         long nano_array1_startTime = System.nanoTime();
@@ -69,29 +70,29 @@ public class BruteForce
         long nano_array2_startTime = System.nanoTime();
         values2 = ob.bruteForce(array2, randomSum);
         long nano_array2_endTime = System.nanoTime();
-        System.out.println("Numbers found in array of 1,000 elements: " + values2[0] + " and " + values2[1] + ".");
-        System.out.println("Elapsed time (ns) for 1,000 elements: " + (nano_array2_endTime-nano_array2_startTime) + "\n");
+        System.out.println("Numbers found in array of 100 elements: " + values2[0] + " and " + values2[1] + ".");
+        System.out.println("Elapsed time (ns) for 100 elements: " + (nano_array2_endTime-nano_array2_startTime) + "\n");
 
         int[] values3 = new int[2];
         long nano_array3_startTime = System.nanoTime();
         values3 = ob.bruteForce(array3, randomSum);
         long nano_array3_endTime = System.nanoTime();
-        System.out.println("Numbers found in array of 10,000 elements: " + values3[0] + " and " + values3[1] + ".");
-        System.out.println("Elapsed time (ns) for 10,000 elements: " + (nano_array3_endTime-nano_array3_startTime) + "\n");
+        System.out.println("Numbers found in array of 100 elements: " + values3[0] + " and " + values3[1] + ".");
+        System.out.println("Elapsed time (ns) for 100 elements: " + (nano_array3_endTime-nano_array3_startTime) + "\n");
 
         int[] values4 = new int[2];
         long nano_array4_startTime = System.nanoTime();
         values4 = ob.bruteForce(array4, randomSum);
         long nano_array4_endTime = System.nanoTime();
-        System.out.println("Numbers found in array of 100,000 elements: " + values4[0] + " and " + values4[1] + ".");
-        System.out.println("Elapsed time (ns) for 100,000 elements: " + (nano_array4_endTime-nano_array4_startTime) + "\n");
+        System.out.println("Numbers found in array of 100 elements: " + values4[0] + " and " + values4[1] + ".");
+        System.out.println("Elapsed time (ns) for 100 elements: " + (nano_array4_endTime-nano_array4_startTime) + "\n");
 
         int[] values5 = new int[2];
         long nano_array5_startTime = System.nanoTime();
         values5 = ob.bruteForce(array5, randomSum);
         long nano_array5_endTime = System.nanoTime();
-        System.out.println("Numbers found in array of 1,000,000 elements: " + values5[0] + " and " + values5[1] + ".");
-        System.out.println("Elapsed time (ns) for 1,000,000 elements: " + (nano_array5_endTime-nano_array5_startTime));
+        System.out.println("Numbers found in array of 00 elements: " + values5[0] + " and " + values5[1] + ".");
+        System.out.println("Elapsed time (ns) for 100 elements: " + (nano_array5_endTime-nano_array5_startTime));
     } // end main
 }
 
